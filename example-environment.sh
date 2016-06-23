39c84423163bb79ccb684baee6536074b0c68ea6
Wesley Hayutin <weshayutin@gmail.com>
2016-06-23 12:06:26 -0400

    define the keys used in the environment.sh file

--- example-environment.sh
+++ example-environment.sh
@@ -3,5 +3,11 @@ export trello_api_key="api_key"
 export BZ_USER=
 export BZ_PASSWORD=
 export RECENT="-2" #Number of weeks from now.  e.g two weeks ago
+
+#key = Red Hat short name
+#trello = Trello Member Name
+#rh_email = Red Hat email
+#openstack = launchpad account email address
+#gerrithub = gerrithub account email address
 export TEAM='{"whayutin":{"trello":"weshayutin1", "rh_email":"foo@redhat.com", "openstack":"foo@gmail.com", "gerrithub":"foo@gmail.com"},
 }'
