00f2498db6a4e4e4a02a4bb1e9203c0e5717961f
Wes Hayutin <weshayutin@gmail.com>
2016-10-19 08:45:01 -0400

    add example team environment

---
+++ example_team.sh
@@ -0,0 +1,10 @@
+#key = Red Hat short name
+#trello = Trello Member Name
+#rh_email = Red Hat email
+#openstack = launchpad account email address
+#gerrithub = gerrithub account email address
+#rdoproject = rdoproject account email address
+
+export TEAM='{"rh_short_name":{"trello": "foo", "rh_email":"foo@redhat.com", "openstack":"foo", "gerrithub":"foo", "rdoproject":"foo"}
+}'
+
